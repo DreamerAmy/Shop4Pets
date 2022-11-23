7e4ac62efd9e88301ee90e61bba70609a23c1334
kailin0216 <jin.ka@northeastern.edu>
2022-11-22 16:25:48 -0800

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import {BrowserRouter} from "react-router-dom";
 
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import ProfileScreen from './screens/ProfileScreen';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import BuyerProfileScreen from "./screens//ProfileScreen/BuyerProfileScreen.js"
+import SellerProfileScreen from "./screens//ProfileScreen/SellerProfileScreen.js"
+import AdminProfileScreen from "./screens//ProfileScreen/AdminProfileScreen.js"
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Routes>
+        <Route path="/profile" element={<ProfileScreen />} />
+        {/* TODO: For profile testing purpose only */}
+        <Route path="/profile/buyer" element={<BuyerProfileScreen />} />
+        <Route path="/profile/seller" element={<SellerProfileScreen />} />
+        <Route path="/profile/admin" element={<AdminProfileScreen />} />
+      </Routes>
     </BrowserRouter>
   );
 }
