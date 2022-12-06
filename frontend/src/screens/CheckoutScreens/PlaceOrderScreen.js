4b7b4eb3dc5d3b059440e35d3a568b2a9421c835
Yingrui (Fifi) Yang <andyfifi6@gmail.com>
2022-12-05 23:05:02 -0800

    Update

--- frontend/src/screens/CheckoutScreens/PlaceOrderScreen.js
+++ frontend/src/screens/CheckoutScreens/PlaceOrderScreen.js
@@ -64,17 +64,17 @@ export default function PlaceOrderScreen() {
         creditCardSecurityCode: cart.paymentMethod.creditCardSecurityCode,
       });
 
-      const { info } = await Axios.post(`${API_BASE}/sellerhist`, {
-            buyerId: currentUser ? currentUser._id : '0',
-            sellerId: cart.cartItems.map((item) => item.sellerId),
-            date: new Date().toISOString().slice(0, 10),
-            receiver: cart.shippingAddress.receiver,
-            address: cart.shippingAddress.address,
-            productBought: cart.cartItems.map((item) => item._id),
-            productQuantity: cart.cartItems.map((item) => item.quantity)
-          }
-
-      );
+      // const { info } = await Axios.post(`${API_BASE}/sellerhist`, {
+      //       buyerId: currentUser ? currentUser._id : '0',
+      //       sellerId: cart.cartItems.map((item) => item.sellerId),
+      //       date: new Date().toISOString().slice(0, 10),
+      //       receiver: cart.shippingAddress.receiver,
+      //       address: cart.shippingAddress.address,
+      //       productBought: cart.cartItems.map((item) => item._id),
+      //       productQuantity: cart.cartItems.map((item) => item.quantity)
+      //     }
+      //
+      // );
       ctxDispatch({ type: 'CART_CLEAR' });
       dispatch({ type: 'CREATE_SUCCESS' });
       localStorage.removeItem('cartItems');
