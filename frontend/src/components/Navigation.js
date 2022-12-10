8d9da8f3fdafdd4ed8d9359ffde05fec1ee5a5de
amyxu <67804304+DreamerAmy@users.noreply.github.com>
2022-12-09 22:05:29 -0800

    update

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,9 +1,74 @@
+<<<<<<< Updated upstream
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
+=======
+import {Badge, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+>>>>>>> Stashed changes
 import SearchBox from "./SearchBox";
 import {Link} from "react-router-dom";
 import './index.css'
+<<<<<<< Updated upstream
 
+=======
+import { logoutThunk } from "../services/UserThunks";
+import {useDispatch, useSelector} from "react-redux";
+
+export default function Navigation() {
+    let { currentUser } = useSelector((state) => state.user)
+    if (currentUser) {
+        sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
+        sessionStorage.setItem('favCount', JSON.stringify(currentUser.favorites.length));
+    }
+    if (!currentUser) {
+        currentUser = JSON.parse(sessionStorage.getItem('currentUser'))
+    }
+    const dispatch = useDispatch()
+    const handleLogout = () => {
+        sessionStorage.removeItem('currentUser');
+        sessionStorage.removeItem('favCount');
+        dispatch(logoutThunk())
+    }
+    return (
+        <Navbar className="nav-bg" expand="lg">
+            <Container>
+                <LinkContainer to="/">
+                    <Navbar.Brand>Barkery</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <SearchBox />
+                    <Nav className="me-auto w-130 justify-content-center">
+
+                        {currentUser ? (
+                            <NavDropdown title={currentUser.name} >
+                                {/*<LinkContainer className="nav-link" to="../profile">*/}
+                                {/*    <NavDropdown.Item>Profile <i className="bi bi-person"></i></NavDropdown.Item>*/}
+                                {/*</LinkContainer>*/}
+                                <Link className="nav-link" >
+                                    <Link to="../profile" href="/" >Profile <i className="bi bi-person"></i></Link>
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link className="nav-link" onClick={handleLogout}>
+                                    Logout
+                                </Link>
+                            </NavDropdown>
+                            ) : (
+                                <Link to="/login" className="nav-link">
+                            Sign In
+                        </Link>)}
+
+                        {/*<Link className="nav-link" onClick={handleLogout}>*/}
+                        {/*    Logout*/}
+                        {/*</Link>*/}
+                    </Nav>
+                    <Nav className="me-auto w-130 justify-content-end">
+                        <Link to="/cart" className="nav-link font-weight-bold">
+                            Cart <i className="bi bi-cart"></i>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+>>>>>>> Stashed changes
 
 export default function Navigation (){
 
---
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,101 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import React, { useEffect, useReducer } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import LoadingBox from "../../components/LoadingBox";
+import MessageBox from "../../components/MessageBox";
+import HomeProduct from "./HomeProduct";
+import axios from "axios";
+import product from "./HomeProduct";
+import ProductItem from "../../components/ProductComponent/ProductItem";
+import { findProductThunk } from "../../services/ProductThunks";
+import './home.css'
+import RecommendProduct from "./RecommendProduct";
+
+
+// Display profile btn only user is logged in
+const profileBtn = (currentUser) => {
+    let profileUrl = "../profile/";
+    if (currentUser) {
+        // profileUrl += currentUser._id;
+        return (
+            <Link className="nav-link" >
+                <Link to={profileUrl} href="/" >View My Profile</Link>
+            </Link>
+        )
+    }
+
+}
+
+
+const Home = () => {
+    let { currentUser } = useSelector((state) => state.user)
+    console.log("home currentUser", currentUser);
+    if (currentUser) {
+        sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
+        sessionStorage.setItem('favCount', JSON.stringify(currentUser.favorites.length));
+    }
+    if (!currentUser) {
+        currentUser = JSON.parse(sessionStorage.getItem('currentUser'))
+    }
+
+
+    const { product } = useSelector((state) => state.product);
+    const dispatch = useDispatch();
+    useEffect(() => { dispatch(findProductThunk()); }, []);
+
+
+    return (
+        <>
+            <Container>
+                <div>
+                    {profileBtn(currentUser)}
+                    {
+                        currentUser &&
+                        <h5>Welcome {currentUser.name}!</h5>
+                    }
+                </div>
+
+                <div>
+                    <img className="img-fluid mt-1 mb-4" src={require("../../images/HomeBanner.png")} alt="Home banner" />
+                </div>
+
+                {
+                    !currentUser &&
+                    <div>
+                        <h4 className="mt-4">Discover Pet Favorites</h4>
+                        <div>
+                            <Row>
+                                {product.map((product) => (
+                                    <Col key={product._id} sm={6} md={4} lg={3} className="mb-3">
+                                        <HomeProduct product={product}></HomeProduct>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    </div>
+                }
+                {
+                    currentUser &&
+                    <div>
+                        <h4 className="mt-4">Recommendation For You</h4>
+                        <div>
+                            <Row>
+                                {product.map((product) => (
+                                    <Col key={product._id} sm={6} md={4} lg={3} className="mb-3">
+                                        <RecommendProduct product={product}></RecommendProduct>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    </div>
+                }
+
+            </Container>
+        </>
+    )
+}
+
+export default Home;
